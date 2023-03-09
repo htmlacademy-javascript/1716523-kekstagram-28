82de10f90d0d8a6ce348a34d3f1302c01fc91ae3
Keks <keks@htmlacademy.ru>
2023-03-09 18:49:50 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,3 @@
+import { photoesDescription } from './data.js';
+
+photoesDescription();
---
+++ 5/js/util.js
@@ -0,0 +1,33 @@
+import {PHOTO_ID_COUNT} from './setup.js';
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function createUniqId (min, max) {
+  const uniqIdArr = [];
+  return function() {
+    while(uniqIdArr.length < max) {
+      const currentValue = getRandomInteger(min, max);
+      if (!uniqIdArr.includes(currentValue)) {
+        uniqIdArr.push(currentValue);
+        return currentValue;
+      }
+    }
+  };
+}
+const photoId = createUniqId(1, PHOTO_ID_COUNT);
+
+function createUniqIdFromGenerator () {
+  let uniqId = 1;
+  return function () {
+    return uniqId++;
+  };
+}
+
+const commentId = createUniqIdFromGenerator();
+
+export {getRandomInteger, photoId, commentId};
