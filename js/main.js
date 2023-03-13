019e43a953f50c4fd3760441193f0083bcccf5ed
Vladimir <yaschuk_vladimir@mail.ru>
2023-03-14 00:21:22 +0300

    create random user-photoes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 import { photoesDescription } from './data.js';
 
-photoesDescription();
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const pictures = photoesDescription();
+
+pictures.forEach((picture) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  const img = pictureElement.querySelector('.picture__img');
+  img.src = picture.url;
+  console.log(picture);
+  picturesContainer.appendChild(pictureElement);
+});
---
+++ js/random-user-pictures.js
@@ -0,0 +1,14 @@
+// import { photoesDescription } from './data.js';
+
+// const picturesContainer = document.querySelector('.pictures');
+// const pictureTemplate = document.querySelector('#picture')
+//   .content
+//   .querySelector('.picture');
+
+// const pictures = photoesDescription();
+
+// pictures.forEach((picture) => {
+//   const pictureElement = pictureTemplate.cloneNode(true);
+//   console.log(picture);
+//   picturesContainer.appendChild(pictureElement);
+// });
