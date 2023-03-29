1f104a086e4ec6cf6f0da3bb2020fca1b3bef4bb
Vladimir <yaschuk_vladimir@mail.ru>
2023-03-29 21:00:32 +0300

    hashtags validate test

--- js/form.js
+++ js/form.js
@@ -5,11 +5,36 @@ const uploadButton = document.querySelector('#upload-file');
 const imageOverlay = document.querySelector('.img-upload__overlay');
 const closeButton = document.querySelector('#upload-cancel');
 const commentField = document.querySelector('.text__description');
-const hashTagField = document.querySelector('text__hashtags');
+const form = document.querySelector('.img-upload__form');
+const hashTagField = document.querySelector('.text__hashtags');
+
+const pristine = new Pristine (form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error',
+});
+
+// const getUniqueTags = (tags) => {
+//   const lowerCaseTags = tags.map((tag) => {
+//     tag.toLowerCase();
+//   });
+//   return lowerCaseTags.length === new Set(lowerCaseTags).size;
+// };
+
+function validateTags (value) {
+  const tags = value.trim().split(' ').filter((tag) => tag.trim().length);
+  console.log(tags, 'xxx');
+}
+
 
 const showImageOverlay = function () {
   imageOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
+  console.log(hashTagField.value);
+  hashTagField.addEventListener('input', () => {
+    validateTags(hashTagField.value);
+  });
+
   document.addEventListener('keydown', (evt) => {
     if (isEscapeKey(evt)) {
       evt.preventDefault();
@@ -45,11 +70,11 @@ commentField.addEventListener('keydown', (evt) => {
   }
 });
 
-// hashTagField.addEventListener('keydown', (evt) => {
-//   if (isEscapeKey(evt)) {
-//     evt.stopPropagation();
-//   }
-// });
+hashTagField.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+});
 
 closeButton.addEventListener('click', closeImageOverlay);
 uploadButton.addEventListener('change', showImageOverlay);
