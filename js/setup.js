cf280aa3b8ef053ab8c957cd23852a370ee6b194
Vladimir <yaschuk_vladimir@mail.ru>
2023-04-14 11:00:06 +0300

    deletes not working functions

--- js/setup.js
+++ js/setup.js
@@ -1,38 +1,6 @@
-const PHOTO_ID_COUNT = 25;
-const MIN_LIKES_COUNT = 15;
-const MAX_LIKES_COUNT = 200;
-const MIN_AVATAR_NUMBER = 1;
-const MAX_AVATAR_NUMBER = 6;
+
 const COMMENT_PER_PORTION = 5;
 const ALERT_SHOW_TIME = 5000;
-const PHOTO_DESCRIPTIONS = [
-  'я на отдыхе',
-  'я на работе',
-  'моё вдохновение',
-  'то что я хочу',
-  'моя цель'
-];
-
-const MESSAGES_ARR = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
-  'В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон',
-];
 
 const imageSizeStep = 25;
 const imageDefaultSize = 100;
@@ -117,5 +85,4 @@ const original = {
 
 const effects = [chrome, sepia, marvin, phobos, heat, original];
 
-export {NAMES, MESSAGES_ARR, PHOTO_DESCRIPTIONS, MAX_AVATAR_NUMBER, MIN_AVATAR_NUMBER,
-  MAX_LIKES_COUNT, MIN_LIKES_COUNT, PHOTO_ID_COUNT, COMMENT_PER_PORTION, effects, imageSizeStep, imageDefaultSize, ALERT_SHOW_TIME};
+export { COMMENT_PER_PORTION, effects, imageSizeStep, imageDefaultSize, ALERT_SHOW_TIME };
--- js/util.js
+++ js/util.js
@@ -1,32 +1,4 @@
-import {PHOTO_ID_COUNT, ALERT_SHOW_TIME} from './setup.js';
-
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-function createUniqId (min, max) {
-  const uniqIdArr = [];
-  return function() {
-    while(uniqIdArr.length < max) {
-      const currentValue = getRandomInteger(min, max);
-      if (!uniqIdArr.includes(currentValue)) {
-        uniqIdArr.push(currentValue);
-        return currentValue;
-      }
-    }
-  };
-}
-const photoId = createUniqId(1, PHOTO_ID_COUNT);
-
-function createUniqIdFromGenerator () {
-  let uniqId = 1;
-  return function () {
-    return uniqId++;
-  };
-}
+import { ALERT_SHOW_TIME } from './setup.js';
 
 function createListItem(avatar, name, message) {
   return `<li class="social__comment">
@@ -39,7 +11,6 @@ function createListItem(avatar, name, message) {
   </li>`;
 }
 
-const commentId = createUniqIdFromGenerator();
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
@@ -72,5 +43,5 @@ const debounce = (callback, timeoutDelay) => {
   };
 };
 
-export {getRandomInteger, photoId, commentId, isEscapeKey, createListItem, showAlert, debounce};
+export { isEscapeKey, createListItem, showAlert, debounce};
 
